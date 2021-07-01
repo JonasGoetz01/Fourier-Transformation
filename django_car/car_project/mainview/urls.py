704e58ee63fec2cc2059bd0819944ff43a87b3e0
JonasGoetz01 <65551807+JonasGoetz01@users.noreply.github.com>
2021-07-01 18:51:38 +0200

    Delete django_car directory

--- django_car/car_project/mainview/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='mainview-home'), #Die Umleitung von der project views datei führt hierher; die Anfrage wird von der Funktion home() in der views Datei ausgeführt
-    path('about/', views.about, name='mainview-about'), #Umleitung an views.py und darin wird die Funktion about() angesprochen
-
-]
--- django_car/car_project/readme.txt
+++
@@ -1,2 +0,0 @@
-server starten: python manage.py runserver
-neue app erstellen: python manage.py startapp appname
